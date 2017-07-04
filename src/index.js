4a0f7f0a02f4b9e8d5a22fcfa02143be410635d9
Mark Nuiver <marknuiver@hoppinger.com>
2017-07-04 16:09:51 +0200

    Working connection with WP API

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import registerServiceWorker from './registerServiceWorker';
 import './build/css/index.css';
 
 ReactDOM.render(
-  <App source="http://dev.drupalreact.com/api/events" event="0"/>,
+  <App source="http://dev.wordpress-react.com/?rest_route=/wp/v2/event" event="0"/>,
   document.getElementById('first')
 );
 ReactDOM.render(
-  <App source="http://dev.drupalreact.com/api/events" event="1" color="tomato" />,
+  <App source="http://dev.wordpress-react.com/?rest_route=/wp/v2/event" event="1"/>,
   document.getElementById('second')
 );
 registerServiceWorker();
