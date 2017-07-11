d379b03f5bb9bb2b62cafa97a1110741d947d870
Mark Nuiver <marknuiver@hoppinger.com>
2017-07-11 09:14:10 +0200

    Played with glamorous vs. traditional styling

--- src/EventDetail.jsx
+++ src/EventDetail.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './assets/images/logo.svg';
+import glamorous from 'glamorous';
+
+const EventDetailWrapper = glamorous.div({
+  backgroundColor: 'white',
+  color: 'tomato'
+});
+
+const EventHeader = glamorous.div({
+  backgroundColor: 'tomato'
+});
 
 class EventDetail extends Component {
-
   constructor(props) {
     super(props);
 
@@ -12,40 +21,50 @@ class EventDetail extends Component {
       date: '',
       location: '',
       organizer: '',
-      description: '',
+      description: ''
     };
   }
 
   componentWillMount() {
     let url = String(this.props.source + this.props.match.params.eventId);
     this.serverRequest = axios.get(url).then(event => {
-      console.log(event.data)
+      console.log(event.data);
       this.setState({
-        title: event.data.title.rendered.replace("&#8217;", "'"),
+        title: event.data.title.rendered.replace('&#8217;', "'"),
         date: event.data.acf.date,
         location: event.data.acf.location,
         organizer: event.data.acf.organizer,
-        description: event.data.acf.description,
+        description: event.data.acf.description
       });
     });
   }
 
   render() {
-    return(
-      <div className="App">
-        <div className="App-header">
+    return (
+      <EventDetailWrapper className="App">
+        <EventHeader className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.state.title}</h2>
-        </div>
+          <h2>
+            {this.state.title}
+          </h2>
+        </EventHeader>
         <div>
-          <h3>{this.state.date}</h3>
-          <p>{this.state.location}</p>
-          <p>{this.state.organizer}</p>
+          <h3>
+            {this.state.date}
+          </h3>
+          <p>
+            {this.state.location}
+          </p>
+          <p>
+            {this.state.organizer}
+          </p>
           {this.state.description}
         </div>
-        <a href="/">Back to overview</a>
-      </div>
-    )
+        <div className="App-button">
+          <a href="/">Back to overview</a>
+        </div>
+      </EventDetailWrapper>
+    );
   }
 }
 
